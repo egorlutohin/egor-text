162bd7fc5258957f4c1f2ccfeb4b158715565a81
Egor Lutohin <egor@lutohin.nsk.ru>
2015-01-21 00:18:55 +0600

    + save and exit labels

--- main.c
+++ main.c
@@ -8,6 +8,10 @@
 
 #include <ncurses.h>
 
+void save() {
+	;
+}
+
 
 void print_usage(char *program_name) {
 	printf("Egor's text editor\n");
@@ -45,6 +49,11 @@ int main(int argc, char **argv) {
 	}
 
 	box(stdscr, 0, 0);
+	#define SAVE_KEY_MSG "F2: Save"
+	#define SEP_KEY_MSG " "
+	#define QUIT_KEY_MSG "F10: Exit"
+	mvprintw(LINES - 1, COLS - 3 - sizeof(SAVE_KEY_MSG) + 1 - sizeof(QUIT_KEY_MSG) + 1 - sizeof(SEP_KEY_MSG) + 1,
+			 " %s%s%s ", SAVE_KEY_MSG, SEP_KEY_MSG, QUIT_KEY_MSG);
 	wrefresh(stdscr);
 
 	noecho();
@@ -59,7 +68,7 @@ int main(int argc, char **argv) {
 
 	int key;
 	int y, x;
-	while((key = getch()) != 27) { // while not pressed ESC
+	while((key = getch()) != KEY_F(10)) { // while not pressed ESC
 
 		switch(key) {
 		case KEY_UP:
@@ -90,6 +99,10 @@ int main(int argc, char **argv) {
 			break;
 
 		case KEY_END:
+			break;
+
+		case KEY_F(2):
+			save();
 			break;
 
 		default:
